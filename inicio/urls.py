d3d13afac0cfde6723815b210bfef6a4283ae9ec
Matias-Levay <mlevay@casasantaisabel.edu.ar>
2022-07-06 21:37:17 -0300

    Created first classes and view about friends, still corrections to make

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from inicio.views import vista_inicio, leerEquipos, equipos, editarEquipo, eliminarEquipo, login_request
+from inicio.views import vista_inicio, leerEquipos, equipos, editarEquipo, eliminarEquipo
 
 urlpatterns = [
     path('', vista_inicio),
@@ -7,5 +7,4 @@ urlpatterns = [
     path('equipos', equipos, name='equipos'),
     path('editarEquipo/<equipo_nombre>/', editarEquipo, name = 'EditarEquipo'),
     path('eliminarEquipo/<equipo_nombre>/', eliminarEquipo, name = 'EliminarEquipo'),
-    path('login', login_request, name = 'Login')
 ]
